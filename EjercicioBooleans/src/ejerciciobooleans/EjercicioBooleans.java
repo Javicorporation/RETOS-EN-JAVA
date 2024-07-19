8bc3c6617d18473450e306db154f7009cef2d10b
Javicorporation <krchipullajavier@gmail.com>
2024-07-19 15:47:17 -0500

    prueba4 Signed-off-by:Javicorporation <krchipullajavier@gmail.com>

--- EjercicioBooleans/src/ejerciciobooleans/EjercicioBooleans.java
+++ EjercicioBooleans/src/ejerciciobooleans/EjercicioBooleans.java
@@ -3,10 +3,7 @@ package ejerciciobooleans;
 public class EjercicioBooleans {
 
     public static void main(String[] args) {
-        System.out.println("hola");
-        int i = 0;
-        System.out.println("es"+i);
-
+        
     }
     
 }
