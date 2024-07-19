61334b8fdb5ca407dee74367a86ab16434b30691
Javicorporation <krchipullajavier@gmail.com>
2024-07-19 15:39:48 -0500

    prueba 2

--- EjercicioBooleans/src/ejerciciobooleans/EjercicioBooleans.java
+++ EjercicioBooleans/src/ejerciciobooleans/EjercicioBooleans.java
@@ -3,7 +3,9 @@ package ejerciciobooleans;
 public class EjercicioBooleans {
 
     public static void main(String[] args) {
-        
+        System.out.println("hola");
+        System.out.println("hola");
+
     }
     
 }
